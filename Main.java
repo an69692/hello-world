85a746da0927dbafab7e9570be1f057eb60c69ae
an69692 <69692123+an69692@users.noreply.github.com>
2020-08-14 11:08:32 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+        System.out.println("My name is Andy Nguyen");
+        System.out.println("I like to play Roblox");
+        System.out.println("I also make games on Roblox");
+        System.out.println("Some afterschool activities I do when the pandemic was not around is taekwondo");
+        System.out.println("I am planning to major in software engineering");
+	}
+}
